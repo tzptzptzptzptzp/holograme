e4a128c5789038c9d5787d53787865a2c21412b6
tzptzptzptzptzp <git@tzp.pink>
2024-06-08 02:50:25 +0900

    ✨ FrameShadowコンポーネントを作成

---
+++ src/components/atoms/FrameShadow/FrameShadow.atom.tsx
@@ -0,0 +1,8 @@
+export const FrameShadow = () => {
+  return (
+    <div
+      className="absolute top-0 right-0 bottom-0 left-0 z-10 w-full h-full rounded-3xl"
+      style={{ boxShadow: "inset 0px 0px 10px 5px rgba(0, 0, 0, 0.3)" }}
+    ></div>
+  );
+};
