72acaa01e6067a0672ee355844adf3137617652b
tzptzptzptzptzp <git@tzp.pink>
2024-06-09 00:58:57 +0900

    ✨ SearchFormコンポーネントを作成

---
+++ src/components/molecules/SearchForm/SearchForm.molecule.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { Button } from "@/components/atoms/Button/Button.atom";
+import { Input } from "@/components/atoms/Input/Input.atom";
+import { Icons } from "@/icons";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  search: string;
+};
+
+export const SearchForm = () => {
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    const query = data.search;
+    const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(
+      query
+    )}`;
+    window.location.href = googleSearchUrl;
+  };
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex gap-[6px] w-full pl-4 pr-3 py-2 bg-white rounded-full"
+    >
+      <Input
+        className="flex-1 w-full"
+        placeholder="Google で 検索"
+        {...register("search")}
+      />
+      <Button className="w-fit" type="submit">
+        <Icons.Search />
+      </Button>
+    </form>
+  );
+};
