95d9692e9473f11a0238ca982230119f8ddf0ed9
tzptzptzptzptzp <git@tzp.pink>
2024-06-10 01:16:14 +0900

    ✨ 各種アイコンを追加

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,5 +1,15 @@
+import { ArchiveBox } from "./ArchiveBox/ArchiveBox.icon";
+import { Chat } from "./Chat/Chat.icon";
+import { ClipBoard } from "./Clipboard/Clipboard.icon";
+import { Config } from "./Config/Config.icon";
+import { Home } from "./Home/Home.icon";
 import { Search } from "./Search/Search.icon";
 
 export const Icons = {
+  ArchiveBox,
+  Chat,
+  ClipBoard,
+  Config,
+  Home,
   Search,
 };
