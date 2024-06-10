b9def815aed59c681e134b1cd6f6f7c00a99111c
tzptzptzptzptzp <git@tzp.pink>
2024-06-11 00:05:47 +0900

    ✨ RecoilRootを設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { RecoilRoot } from "recoil";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <RecoilRoot>
+      <html lang="ja">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </RecoilRoot>
   );
 }
