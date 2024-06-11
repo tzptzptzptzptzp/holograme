ca1c2abb1e05884b1877d0308079fff2f4d34464
tzptzptzptzptzp <git@tzp.pink>
2024-06-12 01:18:13 +0900

    🍱 各種アイコンを追加

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -2,8 +2,10 @@ import { ArchiveBox } from "./ArchiveBox/ArchiveBox.icon";
 import { Chat } from "./Chat/Chat.icon";
 import { ClipBoard } from "./Clipboard/Clipboard.icon";
 import { Config } from "./Config/Config.icon";
+import { Copy } from "./Copy/Copy.icon";
 import { Home } from "./Home/Home.icon";
 import { NewTab } from "./NewTab/NewTab.icon";
+import { Paste } from "./Paste/Paste.icon";
 import { Search } from "./Search/Search.icon";
 
 export const Icons = {
@@ -11,7 +13,9 @@ export const Icons = {
   Chat,
   ClipBoard,
   Config,
+  Copy,
   Home,
   NewTab,
+  Paste,
   Search,
 };
