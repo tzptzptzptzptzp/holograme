0b706d124307b86407bbfbf3c8d981f9ec3e98f2
tzptzptzptzptzp <git@tzp.pink>
2024-06-12 01:07:11 +0900

    ✨ Stateを初期化する処理を追加

--- src/components/templates/Provider/Provider.template.tsx
+++ src/components/templates/Provider/Provider.template.tsx
@@ -1,6 +1,16 @@
 "use client";
 import { RecoilRoot } from "recoil";
+import { useInitialize } from "@/hooks/useInitialize.hook";
 
 export const Provider = ({ children }: { children: React.ReactNode }) => {
-  return <RecoilRoot>{children}</RecoilRoot>;
+  return (
+    <RecoilRoot>
+      <ProviderInner>{children}</ProviderInner>
+    </RecoilRoot>
+  );
+};
+
+const ProviderInner = ({ children }: { children: React.ReactNode }) => {
+  useInitialize();
+  return children;
 };
