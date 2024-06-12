a518261bd2054f73b3f7d3f5514be3c7c0df6c74
tzptzptzptzptzp <git@tzp.pink>
2024-06-13 01:43:59 +0900

    ✨ Toastの初期設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { GlobalFrame } from "@/components/templates/GlobalFrame/GlobalFrame.template";
 import { Provider } from "@/components/templates/Provider/Provider.template";
+
+import "react-toastify/dist/ReactToastify.css";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
