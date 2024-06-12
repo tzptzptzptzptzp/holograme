4b31d3482cf51d773495018c5d6a4f51c4c7dd15
tzptzptzptzptzp <git@tzp.pink>
2024-06-13 01:48:37 +0900

    ⚡️ 処理実行時にトーストを表示するように変更

--- src/components/molecules/ClipboardCopyButton/ClipboardCopyButton.molecule.tsx
+++ src/components/molecules/ClipboardCopyButton/ClipboardCopyButton.molecule.tsx
@@ -1,22 +1,37 @@
 import clsx from "clsx";
+import { toast } from "react-toastify";
 import { Button } from "@/components/atoms/Button/Button.atom";
 import { colorConfig } from "@/config/color.config";
 import { Icons } from "@/icons";
 import { ClipboardMockData } from "@/mock/Clipboard.mock";
+import { textsConfig } from "@/config/texts.config";
+
+const TEXT_LENGTH_LIMIT = 15;
 
 export const ClipboardCopyButton = () => {
   const handleClick = async () => {
     try {
-      const text = await navigator.clipboard.readText();
-      const req = {
-        text,
-        date: Number(new Date()),
-      };
-      ClipboardMockData.push(req);
+      if (ClipboardMockData.length > 0) {
+        const lastItem = ClipboardMockData[ClipboardMockData.length - 1];
+        const textToCopy = lastItem.text;
+        await navigator.clipboard.writeText(textToCopy).then(() => {
+          toast(
+            `${textsConfig.TOAST.CLIPBOARD_SAVE} - ${
+              textToCopy.length >= TEXT_LENGTH_LIMIT
+                ? `${textToCopy.slice(0, TEXT_LENGTH_LIMIT)}...`
+                : textToCopy
+            }`
+          );
+        });
+      } else {
+        toast.error(textsConfig.TOAST.CLIPBOARD_SAVE_ERROR);
+      }
     } catch (err) {
-      console.error("Failed to read clipboard contents: ", err);
+      toast.error(textsConfig.TOAST.CLIPBOARD_SAVE_ERROR);
+      console.error("Failed to write to clipboard: ", err);
     }
   };
+
   return (
     <div className="u-shadow flex items-center p-[3px] border-[3px] border-white rounded-full bg-white bg-opacity-60">
       <Button
