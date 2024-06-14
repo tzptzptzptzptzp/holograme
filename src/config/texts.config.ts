1b6454b28a9386556f4dc8532fc90460174a2ebc
tzptzptzptzptzp <git@tzp.pink>
2024-06-14 22:47:26 +0900

    🔧 メタ情報を追加

--- src/config/texts.config.ts
+++ src/config/texts.config.ts
@@ -1,4 +1,9 @@
 export const textsConfig = {
+  META: {
+    TITLE: "hologra Me",
+    DESCRIPTION:
+      "This web application serves as a next-generation homepage where your avatar supports you within a virtual space. It not only functions as a starting point for browsing but also offers a variety of features such as chat (powered by GPT), text sharing, task management, and note-taking. This innovative platform aims to enhance and streamline your digital life.",
+  },
   TOAST: {
     CLIPBOARD_PASTE: "🦄 美少女ちゃんに渡しました！",
     CLIPBOARD_SAVE: "🦄 美少女ちゃんから受け取りました！",
