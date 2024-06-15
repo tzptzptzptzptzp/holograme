d8886e1d0ec0f63b0173a7a6d53d9f97daeed075
tzptzptzptzptzp <git@tzp.pink>
2024-06-16 01:14:06 +0900

    ✨ セッションをStateに保存する処理を追加

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,15 +1,23 @@
 "use client";
-import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 import { usePathname, useRouter } from "next/navigation";
+import { useSetRecoilState } from "recoil";
 import { GlobalFrame } from "@/components/templates/GlobalFrame/GlobalFrame.template";
 import { useSession } from "@/hooks/useSession.util";
+import { SessionState } from "@/recoil/atoms.recoil";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const path = usePathname();
   const { authStatus, session } = useSession();
 
-  useLayoutEffect(() => {
+  const setSession = useSetRecoilState(SessionState);
+
+  useEffect(() => {
+    setSession(session);
+  }, [session, setSession]);
+
+  useEffect(() => {
     if (authStatus !== "loading" && authStatus === "unauthenticated") {
       router.push("/auth");
     }
