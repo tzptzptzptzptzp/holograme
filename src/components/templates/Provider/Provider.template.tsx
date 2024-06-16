578513fa19e82431a4b734c0af11f89308f3a2eb
tzptzptzptzptzp <git@tzp.pink>
2024-06-17 00:16:13 +0900

    ✨ ReactQueryProviderを設定

--- src/components/templates/Provider/Provider.template.tsx
+++ src/components/templates/Provider/Provider.template.tsx
@@ -1,13 +1,17 @@
 "use client";
 import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useInitialize } from "@/hooks/useInitialize.hook";
 import { useWatch } from "@/hooks/useWatch.hook";
 
 export const Provider = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = new QueryClient();
   return (
-    <RecoilRoot>
-      <ProviderInner>{children}</ProviderInner>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <ProviderInner>{children}</ProviderInner>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 };
 
