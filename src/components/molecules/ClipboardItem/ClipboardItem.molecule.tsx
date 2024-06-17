b491050b9205aacb4b3232e7dd4d6104371f8239
tzptzptzptzptzp <git@tzp.pink>
2024-06-18 02:04:08 +0900

    ✨ クリップボードを取得する処理を追加

--- src/components/molecules/ClipboardItem/ClipboardItem.molecule.tsx
+++ src/components/molecules/ClipboardItem/ClipboardItem.molecule.tsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
 import { Button } from "@/components/atoms/Button/Button.atom";
 import { colorConfig } from "@/config/color.config";
+import { textsConfig } from "@/config/texts.config";
 import { Icons } from "@/icons";
 import { getMaskedText } from "@/utils/getMaskedText.util";
-import { useState } from "react";
+import { getTrimmedText } from "@/utils/GetTrimmedText.util";
 
 const IconSize = 22;
+const TEXT_LENGTH_LIMIT = 15;
 
 export const ClipboardItem = ({
   content,
@@ -19,8 +23,30 @@ export const ClipboardItem = ({
     setIsShow((prev) => !prev);
   };
 
+  const handleClick = async (
+    e: React.MouseEvent<HTMLLIElement, MouseEvent>
+  ) => {
+    if ((e.target as HTMLElement).closest("button")) return;
+    try {
+      await navigator.clipboard.writeText(content).then(() => {
+        toast(
+          `${textsConfig.TOAST.CLIPBOARD_SAVE.SUCCESS} - ${
+            content.length >= TEXT_LENGTH_LIMIT
+              ? getTrimmedText(content, TEXT_LENGTH_LIMIT)
+              : content
+          }`
+        );
+      });
+    } catch (err) {
+      toast.error(textsConfig.TOAST.CLIPBOARD_SAVE.ERROR);
+      console.error("Failed to write to clipboard: ", err);
+    }
+  };
   return (
-    <li className="flex items-center justify-between gap-2 px-4 py-2 rounded-full bg-white bg-opacity-90 relative z-0 cursor-default">
+    <li
+      className="flex items-center justify-between gap-2 px-4 py-2 rounded-full bg-white bg-opacity-90 relative z-0 cursor-default"
+      onClick={handleClick}
+    >
       <p className="truncate">{isShow ? content : getMaskedText(content)}</p>
       <div className="flex items-center gap-3">
         <Button onClick={handleShow}>
