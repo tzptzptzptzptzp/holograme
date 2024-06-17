0e85fe1c9a3d72eafadb6f1381f7083e22cbd60b
tzptzptzptzptzp <git@tzp.pink>
2024-06-18 00:05:58 +0900

    💄 影の仕様を見直し

--- src/components/atoms/Avatar/Avatar.atom.tsx
+++ src/components/atoms/Avatar/Avatar.atom.tsx
@@ -4,12 +4,8 @@ export const Avatar = () => {
   return (
     <Image
       alt="background"
-      className="h-[82vh] w-auto"
+      className="u-shadow-avatar h-[82vh] w-auto"
       src="/bisyojo_chan.png"
-      style={{
-        filter:
-          "drop-shadow(0px 3px 12px rgba(0,0,0,0.5)) drop-shadow(2px 0px 5px rgba(0,0,0,0.3))",
-      }}
       width={800}
       height={800}
     />
--- src/components/molecules/ClipboardCopyButton/ClipboardCopyButton.molecule.tsx
+++ src/components/molecules/ClipboardCopyButton/ClipboardCopyButton.molecule.tsx
@@ -35,7 +35,7 @@ export const ClipboardCopyButton = () => {
   };
 
   return (
-    <div className="u-shadow flex items-center p-[3px] border-[3px] border-white rounded-full bg-white bg-opacity-60">
+    <div className="flex items-center p-[3px] border-[3px] border-white rounded-full bg-white bg-opacity-60">
       <Button
         className={clsx(
           "flex items-center justify-center w-[33px] h-[33px] rounded-full bg-white bg-opacity-50 hover:bg-opacity-100 duration-150"
