67e5b0fb6761c8f8a4b42c0ffd7eb79069f35f85
tzptzptzptzptzp <git@tzp.pink>
2024-06-18 02:19:05 +0900

    ⚡️ HOMEに最新のクリップボードを表示する処理を追加

--- src/components/features/Home/HomeContents.feature.tsx
+++ src/components/features/Home/HomeContents.feature.tsx
@@ -1,15 +1,31 @@
+import { Loader } from "@/components/atoms/Loader/Loader.atom";
 import { ClipboardCopyButton } from "@/components/molecules/ClipboardCopyButton/ClipboardCopyButton.molecule";
+import { ClipboardItem } from "@/components/molecules/ClipboardItem/ClipboardItem.molecule";
 import { ClipboardPasteButton } from "@/components/molecules/ClipboardPasteButton/ClipboardPasteButton.molecule";
 import { SearchForm } from "@/components/molecules/SearchForm/SearchForm.molecule";
 import { SearchTypeSwitcher } from "@/components/molecules/SearchTypeSwitcher/SearchTypeSwitcher.molecule";
+import { useGetClipboard } from "@/hooks/api/useGetClipboard.hook";
 
 export const HomeContents = () => {
+  const { data, isLoading } = useGetClipboard();
+
+  if (isLoading) return <Loader />;
+
   return (
-    <div className="flex gap-3 w-full">
-      <SearchForm />
-      <SearchTypeSwitcher />
-      <ClipboardPasteButton />
-      <ClipboardCopyButton />
+    <div className="flex flex-col gap-3 w-full">
+      <div className="flex gap-3 w-full">
+        <SearchForm />
+        <SearchTypeSwitcher />
+        <ClipboardPasteButton />
+        <ClipboardCopyButton />
+      </div>
+      <ClipboardItem
+        content={data[0].content}
+        id={data[0].id}
+        icon
+        copyIcon={false}
+        deleteIcon={false}
+      />
     </div>
   );
 };
