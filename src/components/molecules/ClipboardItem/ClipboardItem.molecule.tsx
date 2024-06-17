ac00d5379d110a0fca04bf50aa982031290b0fb4
tzptzptzptzptzp <git@tzp.pink>
2024-06-18 02:02:37 +0900

    ⚡️ 表示を切り替える処理を追加

--- src/components/molecules/ClipboardItem/ClipboardItem.molecule.tsx
+++ src/components/molecules/ClipboardItem/ClipboardItem.molecule.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/atoms/Button/Button.atom";
 import { colorConfig } from "@/config/color.config";
 import { Icons } from "@/icons";
 import { getMaskedText } from "@/utils/getMaskedText.util";
+import { useState } from "react";
 
 const IconSize = 22;
 
@@ -12,16 +13,30 @@ export const ClipboardItem = ({
   content: string;
   id: number;
 }) => {
+  const [isShow, setIsShow] = useState(false);
+
+  const handleShow = () => {
+    setIsShow((prev) => !prev);
+  };
+
   return (
     <li className="flex items-center justify-between gap-2 px-4 py-2 rounded-full bg-white bg-opacity-90 relative z-0 cursor-default">
-      <p className="truncate">{getMaskedText(content)}</p>
+      <p className="truncate">{isShow ? content : getMaskedText(content)}</p>
       <div className="flex items-center gap-3">
-        <Button>
-          <Icons.Eye
-            color={colorConfig.text}
-            width={IconSize}
-            height={IconSize}
-          />
+        <Button onClick={handleShow}>
+          {isShow ? (
+            <Icons.EyeSlash
+              color={colorConfig.error}
+              width={IconSize}
+              height={IconSize}
+            />
+          ) : (
+            <Icons.Eye
+              color={colorConfig.text}
+              width={IconSize}
+              height={IconSize}
+            />
+          )}
         </Button>
         <Icons.Copy
           color={colorConfig.success}
