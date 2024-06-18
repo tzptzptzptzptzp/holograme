40a21fda25e0b47d0d8916dcdcbda8f2d84dfaa2
tzptzptzptzptzp <git@tzp.pink>
2024-06-19 00:02:57 +0900

    ✨ HistoryItemコンポーネントを作成

---
+++ src/components/molecules/HistoryItem/HistoryItem.molecule.tsx
@@ -0,0 +1,73 @@
+import { Button } from "@/components/atoms/Button/Button.atom";
+import { colorConfig } from "@/config/color.config";
+import { Icons } from "@/icons";
+
+const IconSize = 22;
+
+type Props = {
+  content: string;
+  deleteIcon?: boolean;
+  icon?: boolean;
+  showIcon?: boolean;
+  newTabIcon?: boolean;
+};
+
+export const HistoryItem = ({
+  content,
+  icon = true,
+  newTabIcon = true,
+  deleteIcon = true,
+}: Props) => {
+  const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(
+    content
+  )}`;
+
+  const handleDelete = () => {};
+
+  const OpenIsCurrentTab = async (
+    e: React.MouseEvent<HTMLLIElement, MouseEvent>
+  ) => {
+    if ((e.target as HTMLElement).closest("button")) return;
+    window.location.href = googleSearchUrl;
+  };
+
+  const OpenIsNewTab = async () => {
+    window.open(googleSearchUrl, "_blank");
+  };
+  return (
+    <li
+      className="flex items-center justify-between gap-1 px-4 py-2 rounded-full bg-white bg-opacity-90 relative z-0 cursor-pointer"
+      onClick={OpenIsCurrentTab}
+    >
+      {icon && (
+        <Icons.Search
+          className="stroke-2"
+          color={colorConfig.primary}
+          width={IconSize}
+          height={IconSize}
+        />
+      )}
+      <p className="w-full text-gray truncate">{content}</p>
+      <div className="flex items-center gap-3">
+        {newTabIcon && (
+          <Button onClick={OpenIsNewTab}>
+            <Icons.NewTab
+              color={colorConfig.success}
+              width={IconSize}
+              height={IconSize}
+            />
+          </Button>
+        )}
+        {deleteIcon && (
+          <Button onClick={handleDelete}>
+            <Icons.Trash
+              color={colorConfig.error}
+              width={IconSize}
+              height={IconSize}
+            />
+          </Button>
+        )}
+      </div>
+    </li>
+  );
+};
