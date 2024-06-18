f626b000c91b710b51b84ca21a705feade3c10e8
tzptzptzptzptzp <git@tzp.pink>
2024-06-19 01:26:13 +0900

    ✨ チャットルーム選択機能を実装

--- src/components/features/Chat/ChatContents.feature.tsx
+++ src/components/features/Chat/ChatContents.feature.tsx
@@ -1,3 +1,39 @@
+import { useEffect, useState } from "react";
+import { Select } from "@/components/atoms/Select/Select.atom";
+import { ContentHead } from "@/components/molecules/ContentHead/ContentHead.molecule";
+import { ContentWrapper } from "@/components/templates/ContentWrapper/ContentWrapper.template";
+import { Icons } from "@/icons";
+
+const options = [
+  { id: 1, name: "オプション01" },
+  { id: 2, name: "オプション02" },
+  { id: 3, name: "オプション03" },
+];
+
 export const ChatContents = () => {
-  return <div>ChatContents</div>;
+  const [currentChatRoomId, setCurrentChatRoomId] = useState<number>(0);
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCurrentChatRoomId(Number(e.target.value));
+  };
+
+  useEffect(() => {
+    setCurrentChatRoomId(options[0].id ?? 0);
+  }, [setCurrentChatRoomId]);
+
+  useEffect(() => {
+    console.log(currentChatRoomId);
+  }, [currentChatRoomId]);
+
+  return (
+    <ContentWrapper>
+      <div className="flex gap-3">
+        <ContentHead>
+          <Icons.Chat color="white" />
+          <p>Chat Room</p>
+          <Select id="chat" onChange={handleChange} options={options} />
+        </ContentHead>
+      </div>
+    </ContentWrapper>
+  );
 };
