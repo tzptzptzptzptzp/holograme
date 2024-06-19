d6decc40991f731c2131b5b262625acea3009659
tzptzptzptzptzp <git@tzp.pink>
2024-06-19 23:39:01 +0900

    ✨ チャットルーム更新APIを作成

--- src/app/api/chat/[id]/route.ts
+++ src/app/api/chat/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { getUserIdFromToken } from "@/app/apiHelpers/getUserIdFromToken.helper";
 import { prisma } from "@/libs/Prisma.lib";
 
-export async function DELETE(
+export async function PUT(
   req: Request,
   { params }: { params: { id: string } }
 ) {
@@ -18,7 +18,52 @@ export async function DELETE(
       where: { id: id },
     });
 
-    console.log(item, userId);
+    if (!item || item.userId !== userId) {
+      return NextResponse.json(
+        { error: "Item not found or unauthorized" },
+        { status: 404 }
+      );
+    }
+
+    const { name } = await req.json();
+
+    const data = await prisma.chatRoom.update({
+      where: { id: id },
+      data: {
+        name: name,
+        updatedDate: new Date(),
+      },
+      select: {
+        id: true,
+        name: true,
+        updatedDate: true,
+      },
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = parseInt(params.id, 10);
+  try {
+    const token = req.headers.get("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const userId = await getUserIdFromToken(token);
+
+    const item = await prisma.chatRoom.findUnique({
+      where: { id: id },
+    });
 
     if (!item || item.userId !== userId) {
       return NextResponse.json(
