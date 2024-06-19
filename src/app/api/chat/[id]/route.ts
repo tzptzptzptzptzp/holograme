800f00eccd4797b8bfff34d3bae2dd7c3e6d3ed7
tzptzptzptzptzp <git@tzp.pink>
2024-06-19 23:52:53 +0900

    ✨ チャットメッセージ取得APIを作成

--- src/app/api/chat/[id]/route.ts
+++ src/app/api/chat/[id]/route.ts
@@ -49,6 +49,41 @@ export async function PUT(
   }
 }
 
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = parseInt(params.id, 10);
+  try {
+    const token = req.headers.get("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const userId = await getUserIdFromToken(token);
+
+    const data = await prisma.chatRoom.findUnique({
+      where: { id: id },
+      include: {
+        messages: true,
+      },
+    });
+
+    if (!data || data.userId !== userId) {
+      return NextResponse.json(
+        { error: "Item not found or unauthorized" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
+
 export async function DELETE(
   req: Request,
   { params }: { params: { id: string } }
