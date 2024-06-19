b77fc531249f32a5240a9d7bad30e813241b9a82
tzptzptzptzptzp <git@tzp.pink>
2024-06-19 22:57:54 +0900

    ✨ チャットルーム一覧取得APIを作成

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -26,3 +26,29 @@ export async function POST(req: Request) {
     );
   }
 }
+
+export async function GET(req: Request) {
+  try {
+    const token = req.headers.get("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const userId = await getUserIdFromToken(token);
+
+    const data = await prisma.chatRoom.findMany({
+      where: {
+        userId: userId,
+      },
+      orderBy: {
+        updatedDate: "desc",
+      },
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
