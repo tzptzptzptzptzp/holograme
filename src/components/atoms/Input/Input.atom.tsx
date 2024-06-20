70c39d24d06bfccc3d2a07906c0303a0258b62c9
tzptzptzptzptzp <git@tzp.pink>
2024-06-21 03:16:35 +0900

    ⚡️ disable時の挙動を追加

--- src/components/atoms/Input/Input.atom.tsx
+++ src/components/atoms/Input/Input.atom.tsx
@@ -3,15 +3,23 @@ import clsx from "clsx";
 
 type Props = {
   className?: string;
+  disabled?: boolean;
   placeholder?: string;
   type?: "text" | "password" | "email";
 };
 
 const Input = forwardRef<HTMLInputElement, Props>(
-  ({ className, placeholder, type = "text", ...props }, ref) => {
+  (
+    { className, disabled = false, placeholder, type = "text", ...props },
+    ref
+  ) => {
     return (
       <input
-        className={clsx("w-full py-[2px] px-1 focus:outline-none", className)}
+        className={clsx(
+          "w-full py-[2px] px-1 focus:outline-none disabled:bg-disableBackground disabled:text-disableText",
+          className
+        )}
+        disabled={disabled}
         placeholder={placeholder}
         type={type}
         ref={ref}
