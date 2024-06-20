e172d59a69f989e70c731f7ecdf86365aee6625c
tzptzptzptzptzp <git@tzp.pink>
2024-06-21 03:03:47 +0900

    🐛 フキダシが見切れる不具合を修正

--- src/components/organisms/ChatRoom/ChatRoom.organism.tsx
+++ src/components/organisms/ChatRoom/ChatRoom.organism.tsx
@@ -18,7 +18,7 @@ export const ChatRoom = ({ roomId }: Props) => {
     );
 
   return (
-    <div className="flex flex-col-reverse gap-2 z-0 overflow-y-scroll h-full max-h-full">
+    <div className="flex flex-col-reverse gap-3 z-0 overflow-y-scroll h-full max-h-full mt-3 pb-3">
       {data.messages.map((message) => (
         <ChatBalloon
           key={message.id}
