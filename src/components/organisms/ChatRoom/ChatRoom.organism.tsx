f1cf626527f71fcd7bf57101e684d547184314c8
tzptzptzptzptzp <git@tzp.pink>
2024-06-22 09:48:22 +0900

    ⚡️ 取得したメッセージをRecoilで管理する処理を追加

--- src/components/organisms/ChatRoom/ChatRoom.organism.tsx
+++ src/components/organisms/ChatRoom/ChatRoom.organism.tsx
@@ -1,16 +1,31 @@
+import { useEffect } from "react";
+import { useRecoilState } from "recoil";
 import { Loader } from "@/components/atoms/Loader/Loader.atom";
 import { ChatBalloon } from "@/components/molecules/ChatBalloon/ChatBalloon.molecule";
 import { CenteringContainer } from "@/components/templates/CenteringContainer/CenteringContainer.template";
 import { useGetChatMessage } from "@/hooks/api/useGetChatMessage.hook";
+import { ChatMessageState } from "@/recoil/atoms.recoil";
 
 type Props = {
   roomId: number;
 };
 
 export const ChatRoom = ({ roomId }: Props) => {
+  const [state, setState] = useRecoilState(ChatMessageState);
+
   const { data, isLoading } = useGetChatMessage(roomId);
 
-  if (isLoading || !data)
+  useEffect(() => {
+    if (isLoading || !data) return;
+    setState({
+      isThinking: false,
+      messages: data.messages.map((message) => ({
+        ...message,
+      })),
+    });
+  }, [data, isLoading, setState]);
+
+  if (isLoading || !data || !state.messages)
     return (
       <CenteringContainer>
         <Loader />
@@ -19,7 +34,7 @@ export const ChatRoom = ({ roomId }: Props) => {
 
   return (
     <div className="flex flex-col-reverse gap-3 z-0 overflow-y-scroll h-full max-h-full mt-3 pb-3">
-      {data.messages.map((message) => (
+      {state.messages.map((message) => (
         <ChatBalloon
           key={message.id}
           message={message.content}
