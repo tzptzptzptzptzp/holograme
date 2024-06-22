45bb8b44177f9065829c09b27b9cc5cf3e1e9498
tzptzptzptzptzp <git@tzp.pink>
2024-06-22 23:20:43 +0900

    🚸 アクティブメニューの視認性を向上

--- src/components/organisms/Navigation/Navigation. organism.tsx
+++ src/components/organisms/Navigation/Navigation. organism.tsx
@@ -1,56 +1,23 @@
 "use client";
-import { useSetRecoilState } from "recoil";
-import { Icons } from "@/icons";
-import { Button } from "../../atoms/Button/Button.atom";
-import { SelectedContentState } from "@/recoil/atoms.recoil";
-import { SelectedContentStateType } from "@/recoil/types.recoil";
-import Image from "next/image";
-import { UserMenu } from "../UserMenu/UserMenu.organism";
 import { useState } from "react";
+import Image from "next/image";
+import { Button } from "@/components/atoms/Button/Button.atom";
+import { NavigationItem } from "@/components/molecules/NavigationItem/NavigationItem.molecule";
+import { UserMenu } from "@/components/organisms/UserMenu/UserMenu.organism";
 
-const IconSize = 32;
 const UserIconSize = 39;
 
 export const Navigation = () => {
   const [isHovered, setIsHovered] = useState(false);
-  const setSelectedContent = useSetRecoilState(SelectedContentState);
-
-  const handleClick = (selectedContent: SelectedContentStateType) => {
-    setSelectedContent(selectedContent);
-  };
 
   return (
     <nav className="u-shadow relative z-30 h-full w-20 bg-red-300">
       <ul className="flex flex-col gap-8 items-center justify-center h-full">
-        <li>
-          <Button onClick={() => handleClick("home")}>
-            <Icons.Home color="white" width={IconSize} height={IconSize} />
-          </Button>
-        </li>
-        <li>
-          <Button onClick={() => handleClick("chat")}>
-            <Icons.Chat color="white" width={IconSize} height={IconSize} />
-          </Button>
-        </li>
-        <li>
-          <Button onClick={() => handleClick("clipboard")}>
-            <Icons.ClipBoard color="white" width={IconSize} height={IconSize} />
-          </Button>
-        </li>
-        <li>
-          <Button onClick={() => handleClick("history")}>
-            <Icons.ArchiveBox
-              color="white"
-              width={IconSize}
-              height={IconSize}
-            />
-          </Button>
-        </li>
-        <li>
-          <Button onClick={() => handleClick("setting")}>
-            <Icons.Config color="white" width={IconSize} height={IconSize} />
-          </Button>
-        </li>
+        <NavigationItem name="home" />
+        <NavigationItem name="chat" />
+        <NavigationItem name="clipboard" />
+        <NavigationItem name="history" />
+        <NavigationItem name="setting" />
       </ul>
       <div
         className="u-centering-x absolute bottom-4"
