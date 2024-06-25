833086c398f109970d14a955c9371885cc99a2f9
tzptzptzptzptzp <git@tzp.pink>
2024-06-25 17:30:14 +0900

    💄 モーダルのスタイルを調整

--- src/components/organisms/Modal/Modal.organism.tsx
+++ src/components/organisms/Modal/Modal.organism.tsx
@@ -1,7 +1,26 @@
+import clsx from "clsx";
+import { useModal } from "@/hooks/useModal.hook";
+import { ModalContentsSwitcher } from "@/utils/ModalContentsSwitcher.util";
+
 export const Modal = () => {
+  const { content, isOpen, handleClose } = useModal();
+
   return (
-    <div className="flex items-center justify-center absolute top-0 right-0 bottom-0 left-0 z-30 bg-black bg-opacity-30">
-      <div className="bg-white min-w-72 min-h-72 max-h-[80%] p-8 rounded-3xl"></div>
+    <div
+      className={clsx(
+        "flex items-center justify-center absolute top-0 right-0 bottom-0 left-0 z-30 p-12 duration-300 transition-all",
+        isOpen
+          ? "opacity-100 pointer-events-auto"
+          : "opacity-0 pointer-events-none"
+      )}
+    >
+      <div
+        className="absolute z-0 w-full h-full bg-black bg-opacity-30 cursor-pointer"
+        onClick={handleClose}
+      ></div>
+      <div className="u-shadow-sm flex items-center justify-center z-10 overflow-x-hidden overflow-y-scroll min-w-72 min-h-72 max-h-[80%] p-8 rounded-3xl bg-white">
+        {ModalContentsSwitcher(content)}
+      </div>
     </div>
   );
 };
