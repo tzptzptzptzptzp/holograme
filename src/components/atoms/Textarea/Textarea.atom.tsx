8beee654cafdd398f527b03fdb2411f9395476b7
tzptzptzptzptzp <git@tzp.pink>
2024-06-27 22:18:07 +0900

    ⚡️ Propsを追加

--- src/components/atoms/Textarea/Textarea.atom.tsx
+++ src/components/atoms/Textarea/Textarea.atom.tsx
@@ -1,31 +1,33 @@
-import { forwardRef, ChangeEvent } from "react";
+import { forwardRef } from "react";
 import clsx from "clsx";
 
 type TextareaProps = {
   className?: string;
+  cols?: number;
   disabled?: boolean;
+  id?: string;
+  onBlur?: (e: React.FocusEvent<HTMLTextAreaElement>) => void;
+  onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  onInput?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  onKeyDown?: (e: React.KeyboardEvent<HTMLTextAreaElement>) => void;
   placeholder?: string;
   rows?: number;
-  cols?: number;
   value?: string;
-  onBlur?: () => void;
-  onChange?: (e: ChangeEvent<HTMLTextAreaElement>) => void;
-  onInput?: () => void;
-  onKeyDown?: (e: React.KeyboardEvent<HTMLTextAreaElement>) => void;
 };
 
 const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
   (
     {
       className,
+      cols,
       disabled = false,
+      id,
       onBlur,
       onChange,
       onInput,
       onKeyDown,
       placeholder,
       rows = 3,
-      cols,
       ...props
     },
     ref
@@ -36,15 +38,16 @@ const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
           "w-full max-h-80 py-[2px] px-1 focus:outline-none disabled:bg-disableBackground disabled:text-disableText resize-none",
           className
         )}
-        disabled={disabled}
-        placeholder={placeholder}
-        rows={rows}
         cols={cols}
+        disabled={disabled}
+        id={id}
         onBlur={onBlur}
         onChange={onChange}
         onInput={onInput}
         onKeyDown={onKeyDown}
+        placeholder={placeholder}
         ref={ref}
+        rows={rows}
         {...props}
       />
     );
