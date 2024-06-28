70e0a79c1ba6cbe7dc59f0d78d551dfd1a687989
tzptzptzptzptzp <git@tzp.pink>
2024-06-29 02:29:35 +0900

    ✨ メモの大枠を作成

--- src/components/features/Memo/MemoContents.feature.tsx
+++ src/components/features/Memo/MemoContents.feature.tsx
@@ -1,3 +1,32 @@
+import { ContentHead } from "@/components/molecules/ContentHead/ContentHead.molecule";
+import { MemoItem } from "@/components/molecules/MemoItem/MemoItem.molecule";
+import { ContentWrapper } from "@/components/templates/ContentWrapper/ContentWrapper.template";
+import { useGetMemo } from "@/hooks/api/useGetMemo.hook";
+import { Icons } from "@/icons";
+
 export const MemoContents = () => {
-  return <div>MemoContents</div>;
+  const { data } = useGetMemo();
+
+  console.log(data);
+
+  return (
+    <ContentWrapper>
+      <div className="flex gap-3">
+        <ContentHead>
+          <Icons.PencilSquare color="white" />
+          <p className="text-white text-[20px] font-bold">Memo</p>
+        </ContentHead>
+      </div>
+      <ul className="flex flex-col gap-3">
+        {data?.map((item) => (
+          <MemoItem
+            key={item.id}
+            content={item.content}
+            title={item.title}
+            id={item.id}
+          />
+        ))}
+      </ul>
+    </ContentWrapper>
+  );
 };
