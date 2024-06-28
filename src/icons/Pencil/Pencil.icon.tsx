caa5669f968dd4ccb8cb3a0e5121a3f02737b7a7
tzptzptzptzptzp <git@tzp.pink>
2024-06-29 02:16:50 +0900

    🍱 アイコンを追加

---
+++ src/icons/Pencil/Pencil.icon.tsx
@@ -0,0 +1,28 @@
+import { colorConfig } from "@/config/color.config";
+import { IconType } from "../icon.type";
+
+export const Pencil = ({
+  className,
+  color = colorConfig.text,
+  width = 24,
+  height = 24,
+}: IconType) => {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke={color}
+      width={width}
+      height={height}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+      />
+    </svg>
+  );
+};
