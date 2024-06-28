5559c9a9e0844e2998dfa9c9bfc8eec35b34778e
tzptzptzptzptzp <git@tzp.pink>
2024-06-29 01:04:41 +0900

    ✨ メモ一覧取得APIを作成

--- src/app/api/memo/route.ts
+++ src/app/api/memo/route.ts
@@ -27,3 +27,29 @@ export async function POST(req: Request) {
     );
   }
 }
+
+export async function GET(req: Request) {
+  try {
+    const token = req.headers.get("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const userId = await getUserIdFromToken(token);
+
+    const data = await prisma.memo.findMany({
+      where: {
+        userId: userId,
+      },
+      orderBy: {
+        date: "desc",
+      },
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
