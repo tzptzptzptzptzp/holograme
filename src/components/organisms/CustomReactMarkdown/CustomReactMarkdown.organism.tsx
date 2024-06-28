b21bed78ae6b51667f83041aa62216259df592fb
tzptzptzptzptzp <git@tzp.pink>
2024-06-29 00:04:12 +0900

    💄 マークダウンのレイアウト調整

--- src/components/organisms/CustomReactMarkdown/CustomReactMarkdown.organism.tsx
+++ src/components/organisms/CustomReactMarkdown/CustomReactMarkdown.organism.tsx
@@ -4,9 +4,29 @@ import { CodeBlock } from "@/components/molecules/CodeBlock/CodeBlock.molecule";
 export const CustomReactMarkdown = ({ markdown }: { markdown: string }) => {
   return (
     <ReactMarkdown
+      className="flex flex-col gap-2 leading-[1.8]"
       components={{
         code: CodeBlock,
-        p: ({ children }) => <div className="leading-[1.8]">{children}</div>,
+        h1: ({ children }) => (
+          <h1 className="text-[1.4em] font-bold">{children}</h1>
+        ),
+        h2: ({ children }) => (
+          <h2 className="text-[1.3em] font-semibold">{children}</h2>
+        ),
+        h3: ({ children }) => (
+          <h3 className="text-[1.2em] font-semibold">{children}</h3>
+        ),
+        h4: ({ children }) => (
+          <h4 className="text-[1.1em] font-semibold">{children}</h4>
+        ),
+        ul: ({ children }) => (
+          <ul className="flex flex-col gap-4">{children}</ul>
+        ),
+        ol: ({ children }) => (
+          <ol className="flex flex-col gap-4">{children}</ol>
+        ),
+        li: ({ children }) => <li className="pl-2">{children}</li>,
+        p: ({ children }) => <div>{children}</div>,
       }}
     >
       {markdown}
