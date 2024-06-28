5d3c0c812666d705fb5619a9e57d71b427ec20cc
tzptzptzptzptzp <git@tzp.pink>
2024-06-29 01:32:06 +0900

    ✨ メモ削除APIを作成

--- src/app/api/memo/[id]/route.ts
+++ src/app/api/memo/[id]/route.ts
@@ -44,3 +44,39 @@ export async function PUT(
     );
   }
 }
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = parseInt(params.id, 10);
+  try {
+    const token = req.headers.get("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const userId = await getUserIdFromToken(token);
+
+    const item = await prisma.memo.findUnique({
+      where: { id: id },
+    });
+
+    if (!item || item.userId !== userId) {
+      return NextResponse.json(
+        { error: "Item not found or unauthorized" },
+        { status: 404 }
+      );
+    }
+
+    const data = await prisma.memo.delete({
+      where: { id: id },
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
