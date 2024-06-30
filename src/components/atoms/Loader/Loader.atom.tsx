249aba63e0cbc23f80207070f7eebe4035b241e3
tzptzptzptzptzp <git@tzp.pink>
2024-06-30 22:58:17 +0900

    ⚡️ ローダーをデフォルトで中央配置するように変更

--- src/components/atoms/Loader/Loader.atom.tsx
+++ src/components/atoms/Loader/Loader.atom.tsx
@@ -1,6 +1,17 @@
 import styles from "./Loader.module.scss";
 
-export const Loader = ({ no = 1 }: { no?: number }) => {
+type Props = {
+  centering?: boolean;
+  no?: number;
+};
+
+export const Loader = ({ centering = true, no = 1 }: Props) => {
   const loaderClass = `loader${no}`;
+  if (centering)
+    return (
+      <div className="flex items-center justify-center w-full h-full">
+        <div className={styles[loaderClass]}></div>;
+      </div>
+    );
   return <div className={styles[loaderClass]}></div>;
 };
