b5f84069444427dcfd71f2b322fff28694712964
tzptzptzptzptzp <git@tzp.pink>
2024-07-01 22:47:43 +0900

    ⚡️ コンテンツの動的読み込みを削除

--- src/utils/ContentsSwitcher.util.tsx
+++ src/utils/ContentsSwitcher.util.tsx
@@ -1,43 +1,10 @@
-import dynamic from "next/dynamic";
-import { Loader } from "@/components/atoms/Loader/Loader.atom";
-import { SelectedContentStateType } from "@/recoil/types.recoil";
 import { HomeContents } from "@/components/features/Home/HomeContents.feature";
-
-const ChatContents = dynamic(
-  () =>
-    import("@/components/features/Chat/ChatContents.feature").then(
-      (mod) => mod.ChatContents
-    ),
-  { loading: () => <Loader /> }
-);
-const ClipboardContents = dynamic(
-  () =>
-    import("@/components/features/Clipboard/ClipboardContents.feature").then(
-      (mod) => mod.ClipboardContents
-    ),
-  { loading: () => <Loader /> }
-);
-const HistoryContents = dynamic(
-  () =>
-    import("@/components/features/History/HistoryContents.feature").then(
-      (mod) => mod.HistoryContents
-    ),
-  { loading: () => <Loader /> }
-);
-const MemoContents = dynamic(
-  () =>
-    import("@/components/features/Memo/MemoContents.feature").then(
-      (mod) => mod.MemoContents
-    ),
-  { loading: () => <Loader /> }
-);
-const SettingContents = dynamic(
-  () =>
-    import("@/components/features/Setting/SettingContents.feature").then(
-      (mod) => mod.SettingContents
-    ),
-  { loading: () => <Loader /> }
-);
+import { ChatContents } from "@/components/features/Chat/ChatContents.feature";
+import { ClipboardContents } from "@/components/features/Clipboard/ClipboardContents.feature";
+import { HistoryContents } from "@/components/features/History/HistoryContents.feature";
+import { MemoContents } from "@/components/features/Memo/MemoContents.feature";
+import { SettingContents } from "@/components/features/Setting/SettingContents.feature";
+import { SelectedContentStateType } from "@/recoil/types.recoil";
 
 export const ContentsSwitcher = (selectedContent: SelectedContentStateType) => {
   switch (selectedContent) {
