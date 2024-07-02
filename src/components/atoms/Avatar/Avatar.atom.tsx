0ff2c01f0a3dddb6804cf04784888f34bf4ded91
tzptzptzptzptzp <git@tzp.pink>
2024-07-02 21:21:20 +0900

    💫 アバターのアニメーションを追加

--- src/components/atoms/Avatar/Avatar.atom.tsx
+++ src/components/atoms/Avatar/Avatar.atom.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import clsx from "clsx";
+import { GetRandomInt } from "@/utils/GetRandomInt.util";
+
+const ANIMATION_INDEX_LIMIT = 12;
 
 export const Avatar = () => {
+  const [animationIndex, setAnimationIndex] = useState("");
+
+  useEffect(() => {
+    const randomIndex = `animate-avatar-${GetRandomInt(
+      1,
+      ANIMATION_INDEX_LIMIT
+    )}`;
+    setAnimationIndex(randomIndex);
+  }, []);
   return (
     <Image
       alt="Avatar"
-      className="u-shadow-avatar h-[82vh] w-auto select-none pointer-events-none"
+      className={clsx(
+        "u-shadow-avatar h-[82vh] w-auto select-none pointer-events-none",
+        "avatar",
+        animationIndex
+      )}
       src="/bisyojo_chan.png"
       width={800}
       height={800}
