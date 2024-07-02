6c98683cd7ba308594239ddbefc00dae00850ca8
tzptzptzptzptzp <git@tzp.pink>
2024-07-02 23:32:43 +0900

    ✨ 設定ページの雛形を作成

--- src/components/features/Setting/SettingContents.feature.tsx
+++ src/components/features/Setting/SettingContents.feature.tsx
@@ -1,3 +1,19 @@
+import { ContentHead } from "@/components/molecules/ContentHead/ContentHead.molecule";
+import { ContentWrapper } from "@/components/templates/ContentWrapper/ContentWrapper.template";
+import { Icons } from "@/icons";
+
 export const SettingContents = () => {
-  return <div>SettingContents</div>;
+  return (
+    <ContentWrapper>
+      <div className="flex gap-3">
+        <ContentHead>
+          <Icons.Config color="white" />
+          <p>Setting</p>
+        </ContentHead>
+      </div>
+      <div>
+        <p>Setting Contents</p>
+      </div>
+    </ContentWrapper>
+  );
 };
