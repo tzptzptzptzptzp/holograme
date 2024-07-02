65408724aef9d7c9f5216404c19ae61de50f1f9a
tzptzptzptzptzp <git@tzp.pink>
2024-07-02 14:57:07 +0900

    🚸 アバターを選択できないように変更

--- src/components/atoms/Avatar/Avatar.atom.tsx
+++ src/components/atoms/Avatar/Avatar.atom.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export const Avatar = () => {
   return (
     <Image
-      alt="background"
-      className="u-shadow-avatar h-[82vh] w-auto"
+      alt="Avatar"
+      className="u-shadow-avatar h-[82vh] w-auto select-none pointer-events-none"
       src="/bisyojo_chan.png"
       width={800}
       height={800}
