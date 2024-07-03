457bedc35798155a6e92225b0d89fa91bb0f76cc
tzptzptzptzptzp <git@tzp.pink>
2024-07-03 13:09:12 +0900

    ✨ ユーザー取得APIを作成

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { getUserIdFromToken } from "@/app/apiHelpers/getUserIdFromToken.helper";
 import { prisma } from "@/libs/Prisma.lib";
 
 export async function POST(req: Request) {
@@ -10,6 +11,29 @@ export async function POST(req: Request) {
         id,
         nickname: "",
         email,
+      },
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(req: Request) {
+  try {
+    const token = req.headers.get("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const userId = await getUserIdFromToken(token);
+
+    const data = await prisma.user.findUnique({
+      where: {
+        id: userId,
       },
     });
 
