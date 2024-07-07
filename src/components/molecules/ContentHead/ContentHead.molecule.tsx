40804179fcf8d8f6f8383207191d45f1449fa716
tzptzptzptzptzp <git@tzp.pink>
2024-07-08 02:01:57 +0900

    📱 チャットの表示を最適化

--- src/components/molecules/ContentHead/ContentHead.molecule.tsx
+++ src/components/molecules/ContentHead/ContentHead.molecule.tsx
@@ -9,7 +9,7 @@ export const ContentHead = ({ children, className }: Props) => {
   return (
     <div
       className={clsx(
-        "flex items-center gap-[6px] w-full pl-4 pr-3 py-2 rounded-full bg-primary text-white text-[20px] font-bold",
+        "flex items-center gap-[6px] w-full pl-4 pr-3 py-2 rounded-full bg-primary text-white text-[20px] s:text-[18px] font-bold",
         className
       )}
     >
--- src/components/templates/ContentWrapper/ContentWrapper.template.tsx
+++ src/components/templates/ContentWrapper/ContentWrapper.template.tsx
@@ -6,7 +6,7 @@ export const ContentWrapper = ({ children, className }: Props) => {
   return (
     <div
       className={clsx(
-        "flex flex-col gap-3 overflow-x-visible overflow-y-scroll w-full h-full pt-16 pb-10",
+        "flex flex-col gap-3 s:gap-2 overflow-x-visible overflow-y-scroll w-full h-full pt-16 pb-10 s:p-0",
         className
       )}
     >
