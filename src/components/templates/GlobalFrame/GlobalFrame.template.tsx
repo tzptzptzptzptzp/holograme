cffdf82e909c8fd004625837de04aecb1e7cb50f
tzptzptzptzptzp <git@tzp.pink>
2024-07-08 01:47:32 +0900

    📱 レスポンシブ調整

--- src/components/templates/GlobalFrame/GlobalFrame.template.tsx
+++ src/components/templates/GlobalFrame/GlobalFrame.template.tsx
@@ -24,14 +24,14 @@ export const GlobalFrame = ({
       <div className="relative overflow-hidden w-full h-full rounded-3xl">
         <Background />
         <div className="flex s:flex-col z-10 w-full h-full">
-          {contents && type !== "SP" && (
-            <div className="flex flex-none items-end relative top-0 h-full pl-4">
+          {contents && type === "PC" && (
+            <div className="flex s:hidden flex-none items-end relative top-0 h-full pl-4">
               <Avatar />
             </div>
           )}
           <main
             className={clsx(
-              "flex flex-col items-center justify-center flex-1 relative top-0 overflow-y-scroll w-full h-full pr-10 pl-4",
+              "flex flex-col items-center justify-center flex-1 s:flex-auto relative top-0 overflow-y-scroll w-full h-full pr-10 pl-4 s:p-4",
               shadow && "u-shadow-sm"
             )}
           >
