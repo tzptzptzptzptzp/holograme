756220e2af56836cc57d4e94c4bfaf46c65f84ad
tzptzptzptzptzp <git@tzp.pink>
2024-07-07 23:55:06 +0900

    📱 表示する内容を変更

--- src/components/organisms/Navigation/Navigation. organism.tsx
+++ src/components/organisms/Navigation/Navigation. organism.tsx
@@ -4,12 +4,14 @@ import Image from "next/image";
 import { Button } from "@/components/atoms/Button/Button.atom";
 import { NavigationItem } from "@/components/molecules/NavigationItem/NavigationItem.molecule";
 import { UserMenu } from "@/components/organisms/UserMenu/UserMenu.organism";
+import { useDevice } from "@/hooks/useDevice.hook";
 
 const UserIconSize = 39;
 
 export const Navigation = () => {
   const [isHovered, setIsHovered] = useState(false);
 
+  const { type } = useDevice();
   return (
     <nav className="u-shadow relative z-30 w-20 s:w-full h-full s:h-auto s:p-2 bg-red-300">
       <ul className="flex flex-col s:flex-row gap-8 s:gap-4 items-center justify-center h-full">
@@ -19,22 +21,34 @@ export const Navigation = () => {
         <NavigationItem name="memo" />
         <NavigationItem name="history" />
         <NavigationItem name="setting" />
+        {type === "SP" && (
+          <Button>
+            <Image
+              alt="美少女ちゃんアイコン"
+              src="/icon.png"
+              width={32}
+              height={32}
+            />
+          </Button>
+        )}
       </ul>
-      <div
-        className="u-centering-x absolute bottom-4"
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
-        <Button>
-          <Image
-            alt="美少女ちゃんアイコン"
-            src="/icon.png"
-            width={UserIconSize}
-            height={UserIconSize}
-          />
-        </Button>
-        <UserMenu isHovered={isHovered} />
-      </div>
+      {type !== "SP" && (
+        <div
+          className="u-centering-x absolute bottom-4"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          <Button>
+            <Image
+              alt="美少女ちゃんアイコン"
+              src="/icon.png"
+              width={UserIconSize}
+              height={UserIconSize}
+            />
+          </Button>
+          <UserMenu isHovered={isHovered} />
+        </div>
+      )}
     </nav>
   );
 };
