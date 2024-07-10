7ec72083582e30e78b546524432d98ff10fc8866
tzptzptzptzptzp <git@tzp.pink>
2024-07-11 00:10:08 +0900

    ⚡️ トークンを自動更新するように変更

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,48 +1,14 @@
 "use client";
 import { useEffect } from "react";
 import { usePathname, useRouter } from "next/navigation";
-import axios from "axios";
-import { useRecoilValue, useSetRecoilState } from "recoil";
 import { GlobalFrame } from "@/components/templates/GlobalFrame/GlobalFrame.template";
-import { useGetChat } from "@/hooks/api/useGetChat.hook";
-import { useGetChatMessage } from "@/hooks/api/useGetChatMessage.hook";
-import { useGetUser } from "@/hooks/api/useGetUser.hook";
 import { useSession } from "@/hooks/useSession.util";
-import {
-  ChatRoomState,
-  FavoriteChatRoomIdState,
-  SessionState,
-} from "@/recoil/atoms.recoil";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const path = usePathname();
 
-  const chatRoom = useRecoilValue(ChatRoomState);
-  const favoriteChatRoomId = useRecoilValue(FavoriteChatRoomIdState);
-
-  const setSession = useSetRecoilState(SessionState);
-
-  const { authStatus, session } = useSession();
-
-  const { refetch: chatRefetch } = useGetChat();
-  const { refetch: chatMessagesRefetch } = useGetChatMessage(
-    favoriteChatRoomId || chatRoom?.id || 0
-  );
-  const { refetch: userRefetch } = useGetUser();
-
-  useEffect(() => {
-    setSession(session);
-    if (session) {
-      axios.defaults.headers.post["Content-Type"] = "application/json";
-      axios.defaults.headers.common[
-        "Authorization"
-      ] = `Bearer ${session.access_token}`;
-      chatRefetch();
-      chatMessagesRefetch();
-      userRefetch();
-    }
-  }, [chatRefetch, chatMessagesRefetch, session, setSession, userRefetch]);
+  const { authStatus } = useSession();
 
   useEffect(() => {
     if (authStatus !== "loading" && authStatus === "unauthenticated") {
