4bf9c404cd51653e5381ce0d3012b684acc8d8a6
tzptzptzptzptzp <git@tzp.pink>
2024-07-11 03:16:53 +0900

    🔥 ルーティングを削除

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,20 +1,12 @@
 "use client";
-import { useEffect } from "react";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { GlobalFrame } from "@/components/templates/GlobalFrame/GlobalFrame.template";
 import { useSession } from "@/hooks/useSession.util";
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
   const path = usePathname();
 
   const { authStatus } = useSession();
-
-  useEffect(() => {
-    if (authStatus !== "loading" && authStatus === "unauthenticated") {
-      process.env.NODE_ENV === "production" && router.push("/auth");
-    }
-  }, [authStatus, router]);
 
   return (
     <GlobalFrame
