546aeb8f8fbb47030e90b3168dc5d725cf6e07dd
tzptzptzptzptzp <git@tzp.pink>
2024-07-13 02:42:00 +0900

    ✨ メッセージをコピーする機能を追加

--- src/components/molecules/ChatBalloon/ChatBalloon.molecule.tsx
+++ src/components/molecules/ChatBalloon/ChatBalloon.molecule.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import clsx from "clsx";
 import styles from "./ChatBalloon.module.scss";
+import { Button } from "@/components/atoms/Button/Button.atom";
 import { CustomReactMarkdown } from "@/components/organisms/CustomReactMarkdown/CustomReactMarkdown.organism";
+import { colorConfig } from "@/config/color.config";
+import { Icons } from "@/icons";
 
 type Props = {
   message: string;
@@ -8,8 +12,28 @@ type Props = {
 };
 
 export const ChatBalloon = ({ message, role }: Props) => {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const handleClose = () => {
+    setMenuVisible(false);
+  };
+
+  const handleContextMenu = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    setMenuVisible(true);
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(message);
+    setMenuVisible(false);
+  };
   return (
-    <div className={clsx(styles.balloon, styles[role])}>
+    <div
+      className={clsx(styles.balloon, styles[role])}
+      onContextMenu={(e) => handleContextMenu(e)}
+    >
       <div className={clsx(styles.inner, styles[role])}>
         <div
           className={clsx(
@@ -19,6 +43,32 @@ export const ChatBalloon = ({ message, role }: Props) => {
         >
           <CustomReactMarkdown markdown={message} />
         </div>
+        {menuVisible && (
+          <div className={clsx(styles.contextMenu, styles[role])}>
+            <Button
+              className="flex items-center font-bold"
+              onClick={handleCopy}
+            >
+              全文コピー
+              <Icons.Copy
+                className="stroke-2"
+                color={role === "user" ? colorConfig.text : "white"}
+              />
+            </Button>
+            <Button
+              className="hover:opacity-70"
+              hover={false}
+              onClick={handleClose}
+            >
+              <Icons.XMark
+                className="absolute top-2 right-4 stroke-2"
+                color={role === "user" ? colorConfig.text : "white"}
+                width={28}
+                height={28}
+              />
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
