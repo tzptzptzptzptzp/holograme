1dfdb062a05e20ef7db6624e744356102b35fefc
tzptzptzptzptzp <git@tzp.pink>
2024-07-12 23:50:05 +0900

    🔥 不要な記述を削除

--- src/libs/supabase/middleware.lib.ts
+++ src/libs/supabase/middleware.lib.ts
@@ -33,11 +33,7 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
-  ) {
+  if (!user && !request.nextUrl.pathname.startsWith("/auth")) {
     const url = request.nextUrl.clone();
     url.pathname = "/auth";
     return NextResponse.redirect(url);
