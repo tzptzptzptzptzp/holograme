475a520a24a0faaaeb21b20289b1b6ffef23de21
tzptzptzptzptzp <git@tzp.pink>
2024-07-12 23:20:39 +0900

    🧐 ミドルウェアを一時的に初期化

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,21 +2,21 @@ import { NextRequest, NextResponse } from "next/server";
 import supabase from "./libs/SupabaseClient.lib";
 
 export async function middleware(req: NextRequest) {
-  const token = req.cookies.get("supabase-auth-token")?.value;
+  // const token = req.cookies.get("supabase-auth-token")?.value;
 
-  if (!token && !req.nextUrl.pathname.startsWith("/auth")) {
-    return NextResponse.redirect(new URL("/auth", req.url));
-  }
+  // if (!token && !req.nextUrl.pathname.startsWith("/auth")) {
+  //   return NextResponse.redirect(new URL("/auth", req.url));
+  // }
 
-  const { data: session } = await supabase.auth.getSession();
+  // const { data: session } = await supabase.auth.getSession();
 
-  if (token && req.nextUrl.pathname.startsWith("/auth")) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
+  // if (token && req.nextUrl.pathname.startsWith("/auth")) {
+  //   return NextResponse.redirect(new URL("/", req.url));
+  // }
 
-  if (!session && req.nextUrl.pathname !== "/auth") {
-    return NextResponse.redirect(new URL("/auth", req.url));
-  }
+  // if (!session && req.nextUrl.pathname !== "/auth") {
+  //   return NextResponse.redirect(new URL("/auth", req.url));
+  // }
 
   return NextResponse.next();
 }
