b113f5381a6d567fb154e747b5b5fda6d598aa9f
tzptzptzptzptzp <git@tzp.pink>
2024-07-14 01:41:07 +0900

    ✨ 定型文取得APIを作成

--- src/app/api/chat/standard-phrase/route.ts
+++ src/app/api/chat/standard-phrase/route.ts
@@ -27,3 +27,26 @@ export async function POST(req: Request) {
     );
   }
 }
+
+export async function GET(req: Request) {
+  try {
+    const token = req.headers.get("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const userId = await getUserIdFromToken(token);
+
+    const data = await prisma.chatStandardPhrase.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
