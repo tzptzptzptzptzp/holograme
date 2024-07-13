436ae96d6c9de8136eba0c0fb3d6af7beede381b
tzptzptzptzptzp <git@tzp.pink>
2024-07-14 02:19:35 +0900

    ✨ 定型文取得フックを作成

---
+++ src/hooks/api/useGetChatStandardPhrase.hook.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { ChatStandardPhrase } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
+import { queryKeysConfig } from "@/config/queryKeys.config";
+import { GetMinutesToMilliseconds } from "@/utils/GetMinutesToMilliseconds.util";
+
+const getChatStandardPhrase = async () => {
+  if (!axios.defaults.headers.common["Authorization"]) {
+    throw new Error("Authorization token is missing");
+  }
+  const res = await axios.get<ChatStandardPhrase[]>(
+    "/api/chat/standard-phrase"
+  );
+  return res.data;
+};
+
+export const useGetChatStandardPhrase = () => {
+  return useQuery({
+    queryKey: [queryKeysConfig.GET_CHAT_STANDARD_PHRASE],
+    queryFn: getChatStandardPhrase,
+    enabled: !!axios.defaults.headers.common["Authorization"],
+    staleTime: GetMinutesToMilliseconds(60),
+  });
+};
