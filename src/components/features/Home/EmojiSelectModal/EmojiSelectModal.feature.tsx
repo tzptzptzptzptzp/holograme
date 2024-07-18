3401da04703f8ada009e7b35b7eb7a786b754e90
tzptzptzptzptzp <git@tzp.pink>
2024-07-18 21:29:51 +0900

    ✨ 選択した絵文字を反映する処理を追加

--- src/components/features/Home/EmojiSelectModal/EmojiSelectModal.feature.tsx
+++ src/components/features/Home/EmojiSelectModal/EmojiSelectModal.feature.tsx
@@ -1,11 +1,33 @@
 import data from "@emoji-mart/data";
 import Picker from "@emoji-mart/react";
+import { useSetRecoilState } from "recoil";
 import { ModalInner } from "@/components/templates/ModalInner/ModalInner.template";
+import { useModal } from "@/hooks/useModal.hook";
+import { CreateFavoriteState } from "@/recoil/atoms.recoil";
+
+type EmojiType = {
+  id: string;
+  native: string;
+  unified: string;
+};
 
 export const EmojiSelectModal = () => {
+  const setCreateFavorite = useSetRecoilState(CreateFavoriteState);
+
+  const { handleOpen } = useModal();
+
+  const handleSelect = (emoji: EmojiType) => {
+    setCreateFavorite((prev) => ({
+      ...prev,
+      emojiId: emoji.id,
+      emojiNative: emoji.native,
+      emojiUnified: emoji.unified,
+    }));
+    handleOpen("createFavorite");
+  };
   return (
     <ModalInner enableButton={false}>
-      <Picker data={data} onEmojiSelect={console.log} />
+      <Picker data={data} onEmojiSelect={handleSelect} />
     </ModalInner>
   );
 };
