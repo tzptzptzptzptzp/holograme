4a80f538075ce2f6e1429934da6a127fc33bab12
tzptzptzptzptzp <git@tzp.pink>
2024-07-19 23:04:04 +0900

    ✨ お気に入り更新APIを作成

--- src/app/api/favorite/[id]/route.ts
+++ src/app/api/favorite/[id]/route.ts
@@ -2,6 +2,51 @@ import { NextResponse } from "next/server";
 import { getUserIdFromToken } from "@/app/apiHelpers/getUserIdFromToken.helper";
 import { prisma } from "@/libs/Prisma.lib";
 
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = parseInt(params.id, 10);
+  try {
+    const token = req.headers.get("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const userId = await getUserIdFromToken(token);
+    const { title, url, emojiId, emojiNative, emojiUnified } = await req.json();
+
+    const item = await prisma.favorite.findUnique({
+      where: { id: id },
+    });
+
+    if (!item || item.userId !== userId) {
+      return NextResponse.json(
+        { error: "Item not found or unauthorized" },
+        { status: 404 }
+      );
+    }
+
+    const data = await prisma.favorite.update({
+      where: { id: id },
+      data: {
+        ...item,
+        title,
+        url,
+        emojiId,
+        emojiNative,
+        emojiUnified,
+      },
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
+
 export async function DELETE(
   req: Request,
   { params }: { params: { id: string } }
