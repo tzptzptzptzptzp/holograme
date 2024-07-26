c31015465a2a16eae5d51a244bda1052dddc3509
tzptzptzptzptzp <git@tzp.pink>
2024-07-26 23:20:35 +0900

    🌱 初期値を変更

--- src/recoil/default.recoil.ts
+++ src/recoil/default.recoil.ts
@@ -1,4 +1,73 @@
-import { ModelStateType } from "./types.recoil";
+import {
+  ChatMessageStateType,
+  ChatRoomOptionStateType,
+  ChatRoomStateType,
+  CreateFavoriteStateType,
+  EditChatStandardPhraseStateType,
+  EditFavoriteStateType,
+  ModalStateType,
+  ModelStateType,
+  UserStateType,
+} from "./types.recoil";
+
+export const UserDefaultValue: UserStateType = {
+  id: "",
+  username: "",
+  nickname: "",
+  email: "",
+  location: "",
+  createdDate: new Date(),
+  updatedDate: new Date(),
+};
+
+export const ChatRoomDefaultValue: ChatRoomStateType = {
+  id: 0,
+  name: "",
+  description: "",
+  defaultMessage: "",
+};
+
+export const ChatRoomOptionsDefaultValue: ChatRoomOptionStateType[] = [
+  {
+    id: 0,
+    name: "",
+  },
+];
+
+export const ChatMessagesDefaultValue: ChatMessageStateType[] = [
+  {
+    id: 0,
+    userId: "",
+    roomId: 0,
+    content: "",
+    role: "user",
+    date: new Date(),
+  },
+];
+
+export const EditChatStandardPhraseDefaultValue: EditChatStandardPhraseStateType =
+  {
+    id: 0,
+    title: "",
+    content: "",
+  };
+
+export const CreateFavoriteDefaultValue: CreateFavoriteStateType = {
+  title: "",
+  url: "",
+  emojiId: "star",
+  emojiNative: "⭐",
+  emojiUnified: "2b50",
+};
+
+export const EditFavoriteDefaultValue: EditFavoriteStateType = {
+  id: 0,
+  title: "",
+  url: "",
+  emojiId: "star",
+  emojiNative: "⭐",
+  emojiUnified: "2b50",
+};
 
 export const ModelsDefaultValue: ModelStateType[] = [
   {
@@ -8,3 +77,8 @@ export const ModelsDefaultValue: ModelStateType[] = [
     owned_by: "",
   },
 ];
+
+export const ModalDefaultValue: ModalStateType = {
+  content: "",
+  isOpen: false,
+};
