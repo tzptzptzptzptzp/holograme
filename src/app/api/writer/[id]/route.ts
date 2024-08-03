91ae6ed9db02e507d8f4e72cbb5ab2a491df68cd
tzptzptzptzptzp <git@tzp.pink>
2024-08-03 15:56:39 +0900

    ✨ ライター削除APIを作成

--- src/app/api/writer/[id]/route.ts
+++ src/app/api/writer/[id]/route.ts
@@ -55,3 +55,39 @@ export async function PUT(
     );
   }
 }
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = parseInt(params.id, 10);
+  try {
+    const token = req.headers.get("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const userId = await getUserIdFromToken(token);
+
+    const item = await prisma.writer.findUnique({
+      where: { id: id, userId: userId },
+    });
+
+    if (!item || item.userId !== userId) {
+      return NextResponse.json(
+        { error: "Item not found or unauthorized" },
+        { status: 404 }
+      );
+    }
+
+    const data = await prisma.writer.delete({
+      where: { id: id, userId: userId },
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
