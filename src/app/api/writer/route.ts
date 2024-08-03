80256cdda1560a77e84c4bc46c1fc798a4cd67d7
tzptzptzptzptzp <git@tzp.pink>
2024-08-03 14:29:37 +0900

    ✨ ライター登録APIを作成

---
+++ src/app/api/writer/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import { getUserIdFromToken } from "../../apiHelpers/getUserIdFromToken.helper";
+import { prisma } from "../../../libs/Prisma.lib";
+
+export async function POST(req: Request) {
+  try {
+    const token = req.headers.get("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const userId = await getUserIdFromToken(token);
+    const {
+      name,
+      expertise,
+      targetAudience,
+      sitePurpose,
+      siteGenre,
+      toneAndStyle,
+    } = await req.json();
+
+    const data = await prisma.writer.create({
+      data: {
+        userId,
+        name,
+        expertise,
+        targetAudience,
+        sitePurpose,
+        siteGenre,
+        toneAndStyle,
+      },
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
