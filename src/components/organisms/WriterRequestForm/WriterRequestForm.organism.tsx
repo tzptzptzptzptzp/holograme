ed514d51c84cbf606c24016661faddb1419c8a14
tzptzptzptzptzp <git@tzp.pink>
2024-08-05 02:24:09 +0900

    ✨ フォームの雛形を作成

---
+++ src/components/organisms/WriterRequestForm/WriterRequestForm.organism.tsx
@@ -0,0 +1,8 @@
+export const WriterRequestForm = () => {
+  return (
+    <div className="flex flex-col gap-4 px-6 py-4 rounded-3xl bg-white bg-opacity-90 relative z-0">
+      <h2 className="text-[18px]">どんな記事を書く？</h2>
+      <form className="flex flex-col gap-4"></form>
+    </div>
+  );
+};
