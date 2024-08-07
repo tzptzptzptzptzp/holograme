3a7c1f536e533e23954bc6d33601b191e8ef661e
tzptzptzptzptzp <git@tzp.pink>
2024-08-08 01:15:40 +0900

    ✨ FormSelectコンポーネントを作成

--- src/components/atoms/Select/Select.atom.tsx
+++ src/components/atoms/Select/Select.atom.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from "react";
 import { cn } from "@/utils/cn.util";
 
 type Option = {
@@ -7,29 +8,36 @@ type Option = {
 
 type Props = {
   className?: string;
+  disabled?: boolean;
   id: string;
-  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
   options: Option[];
   value?: string | number;
 };
 
-export const Select = ({ className, id, onChange, options, value }: Props) => {
-  return (
-    <select
-      className={cn(
-        "w-full max-w-fit text-right truncate focus-visible:outline-none",
-        className
-      )}
-      id={id}
-      name={id}
-      onChange={onChange}
-      value={value}
-    >
-      {options.map((option) => (
-        <option key={option.id} value={option.id}>
-          {option.name}
-        </option>
-      ))}
-    </select>
-  );
-};
+export const Select = forwardRef<HTMLSelectElement, Props>(
+  ({ className, disabled, id, onChange, options, value }, ref) => {
+    return (
+      <select
+        className={cn(
+          "w-full max-w-fit text-right truncate focus-visible:outline-none",
+          className
+        )}
+        disabled={disabled}
+        id={id}
+        name={id}
+        onChange={onChange}
+        value={value}
+        ref={ref}
+      >
+        {options.map((option) => (
+          <option key={option.id} value={option.id}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+    );
+  }
+);
+
+Select.displayName = "Select";
---
+++ src/components/forms/FormSelect/FormSelect.form.tsx
@@ -0,0 +1,76 @@
+import { forwardRef } from "react";
+import { Select } from "@/components/atoms/Select/Select.atom";
+import { ErrorMessage } from "../ErrorMessage/ErrorMessage.form";
+import { cn } from "@/utils/cn.util";
+import { GenerateRandomID } from "@/utils/GenerateRandomID.util";
+
+type Option = {
+  id: number;
+  name: string;
+};
+
+type Props = {
+  disabled?: boolean;
+  errorMessage?: string;
+  selectClassName?: string;
+  label?: string;
+  labelClassName?: string;
+  onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  options: Option[];
+  placeholder?: string;
+  value?: string;
+  wrapperClassName?: string;
+};
+
+const FormSelect = forwardRef<HTMLSelectElement, Props>(
+  (
+    {
+      disabled = false,
+      errorMessage,
+      selectClassName,
+      label,
+      labelClassName,
+      onChange,
+      options,
+      placeholder,
+      value,
+      wrapperClassName,
+      ...props
+    },
+    ref
+  ) => {
+    const id = GenerateRandomID();
+    return (
+      <div className={cn("flex flex-col", wrapperClassName)}>
+        <div className="flex flex-col">
+          {label && (
+            <label
+              className={cn("px-1 text-[12px]", labelClassName)}
+              htmlFor={id}
+            >
+              {label}
+            </label>
+          )}
+          <Select
+            className={cn(
+              "max-w-none min-w-[390px] s:min-w-0 py-[2px] px-1 border-b-2 border-text bg-white bg-opacity-50 text-left",
+              selectClassName
+            )}
+            disabled={disabled}
+            id={id}
+            onChange={onChange}
+            options={options}
+            ref={ref}
+            value={value}
+            {...props}
+          />
+        </div>
+        {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+      </div>
+    );
+  }
+);
+
+FormSelect.displayName = "FormSelect";
+
+export { FormSelect };
