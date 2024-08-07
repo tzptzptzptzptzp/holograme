ce6cf82b4901a69ca3a11a8c710ecb5fe6994581
tzptzptzptzptzp <git@tzp.pink>
2024-08-08 01:19:33 +0900

    ✨ 記事作成フォームを作成

--- src/components/organisms/WriterRequestForm/WriterRequestForm.organism.tsx
+++ src/components/organisms/WriterRequestForm/WriterRequestForm.organism.tsx
@@ -1,6 +1,8 @@
 import { useForm } from "react-hook-form";
 import { Writer } from "@prisma/client";
 import { FormInput } from "@/components/forms/FormInput/FormInput.form";
+import { FormTextarea } from "@/components/forms/FormTextarea/FormTextarea.form";
+import { FormSelect } from "@/components/forms/FormSelect/FormSelect.form";
 import { textsConfig } from "@/config/texts.config";
 import { GetRequiredMessage } from "@/utils/GetRequiredMessage.util";
 
@@ -46,6 +48,169 @@ export const WriterRequestForm = ({ writer }: Props) => {
       </div>
       <form className="flex flex-col gap-4">
         <div className="flex flex-col gap-2">
+          <p>{textsConfig.FORM.WRITER_REQUEST.SECTIONS[0]}</p>
+          <div className="flex gap-4">
+            <FormInput
+              inputClassName={inputClassName}
+              wrapperClassName={wrapperClassName}
+              label={textsConfig.FORM.WRITER_REQUEST.INPUTS.TITLE}
+              errorMessage={errors.title?.message}
+              placeholder={`${textsConfig.FORM.WRITER_REQUEST.INPUTS.TITLE}を入力`}
+              {...register("title", {
+                required: GetRequiredMessage(
+                  textsConfig.FORM.WRITER_REQUEST.INPUTS.TITLE
+                ),
+              })}
+            />
+            <FormInput
+              inputClassName={inputClassName}
+              wrapperClassName={wrapperClassName}
+              label={textsConfig.FORM.WRITER_REQUEST.INPUTS.WORD_COUNT}
+              errorMessage={errors.word_count?.message}
+              placeholder={`${textsConfig.FORM.WRITER_REQUEST.INPUTS.WORD_COUNT}を入力`}
+              {...register("word_count", {
+                required: GetRequiredMessage(
+                  textsConfig.FORM.WRITER_REQUEST.INPUTS.WORD_COUNT
+                ),
+              })}
+            />
+          </div>
+          <FormInput
+            inputClassName={inputClassName}
+            wrapperClassName={wrapperClassName}
+            label={textsConfig.FORM.WRITER_REQUEST.INPUTS.SUMMARY}
+            errorMessage={errors.summary?.message}
+            placeholder={`${textsConfig.FORM.WRITER_REQUEST.INPUTS.SUMMARY}を入力`}
+            {...register("summary", {
+              required: GetRequiredMessage(
+                textsConfig.FORM.WRITER_REQUEST.INPUTS.SUMMARY
+              ),
+            })}
+          />
+          <p>{textsConfig.FORM.WRITER_REQUEST.SECTIONS[1]}</p>
+          <FormInput
+            inputClassName={inputClassName}
+            wrapperClassName={wrapperClassName}
+            label={textsConfig.FORM.WRITER_REQUEST.INPUTS.KEYWORDS}
+            errorMessage={errors.keywords?.message}
+            placeholder={`${textsConfig.FORM.WRITER_REQUEST.INPUTS.KEYWORDS}を入力`}
+            {...register("keywords", {
+              required: GetRequiredMessage(
+                textsConfig.FORM.WRITER_REQUEST.INPUTS.KEYWORDS
+              ),
+            })}
+          />
+          <FormTextarea
+            textareaClassName={inputClassName}
+            wrapperClassName={wrapperClassName}
+            label={
+              textsConfig.FORM.WRITER_REQUEST.INPUTS.STRUCTURE_AND_HEADINGS
+            }
+            errorMessage={errors.structureAndHeadings?.message}
+            placeholder={`${textsConfig.FORM.WRITER_REQUEST.INPUTS.STRUCTURE_AND_HEADINGS}を入力`}
+            {...register("structureAndHeadings", {
+              required: GetRequiredMessage(
+                textsConfig.FORM.WRITER_REQUEST.INPUTS.STRUCTURE_AND_HEADINGS
+              ),
+            })}
+          />
+          <div className="flex gap-4">
+            <FormInput
+              inputClassName={inputClassName}
+              wrapperClassName={wrapperClassName}
+              label={textsConfig.FORM.WRITER_REQUEST.INPUTS.PRODUCT_INFO}
+              errorMessage={errors.productInfo?.message}
+              placeholder={`${textsConfig.FORM.WRITER_REQUEST.INPUTS.PRODUCT_INFO}を入力`}
+              {...register("productInfo", {
+                required: GetRequiredMessage(
+                  textsConfig.FORM.WRITER_REQUEST.INPUTS.PRODUCT_INFO
+                ),
+              })}
+            />
+            <FormInput
+              inputClassName={inputClassName}
+              wrapperClassName={wrapperClassName}
+              label={textsConfig.FORM.WRITER_REQUEST.INPUTS.PRODUCT_URL}
+              errorMessage={errors.productUrl?.message}
+              placeholder={`${textsConfig.FORM.WRITER_REQUEST.INPUTS.PRODUCT_URL}を入力`}
+              {...register("productUrl", {
+                required: GetRequiredMessage(
+                  textsConfig.FORM.WRITER_REQUEST.INPUTS.PRODUCT_URL
+                ),
+              })}
+            />
+          </div>
+          <div className="flex gap-4">
+            <FormInput
+              inputClassName={inputClassName}
+              wrapperClassName={wrapperClassName}
+              label={
+                textsConfig.FORM.WRITER_REQUEST.INPUTS.REVENUE_ARTICLE_TITLE
+              }
+              errorMessage={errors.revenueArticleTitle?.message}
+              placeholder={`${textsConfig.FORM.WRITER_REQUEST.INPUTS.REVENUE_ARTICLE_TITLE}を入力`}
+              {...register("revenueArticleTitle", {
+                required: GetRequiredMessage(
+                  textsConfig.FORM.WRITER_REQUEST.INPUTS.REVENUE_ARTICLE_TITLE
+                ),
+              })}
+            />
+            <FormInput
+              inputClassName={inputClassName}
+              wrapperClassName={wrapperClassName}
+              label={textsConfig.FORM.WRITER_REQUEST.INPUTS.REVENUE_ARTICLE_URL}
+              errorMessage={errors.revenueArticleUrl?.message}
+              placeholder={`${textsConfig.FORM.WRITER_REQUEST.INPUTS.REVENUE_ARTICLE_URL}を入力`}
+              {...register("revenueArticleUrl", {
+                required: GetRequiredMessage(
+                  textsConfig.FORM.WRITER_REQUEST.INPUTS.REVENUE_ARTICLE_URL
+                ),
+              })}
+            />
+          </div>
+          <FormInput
+            inputClassName={inputClassName}
+            wrapperClassName={wrapperClassName}
+            label={
+              textsConfig.FORM.WRITER_REQUEST.INPUTS.REVENUE_ARTICLE_SUMMARY
+            }
+            errorMessage={errors.revenueArticleSummary?.message}
+            placeholder={`${textsConfig.FORM.WRITER_REQUEST.INPUTS.REVENUE_ARTICLE_SUMMARY}を入力`}
+            {...register("revenueArticleSummary", {
+              required: GetRequiredMessage(
+                textsConfig.FORM.WRITER_REQUEST.INPUTS.REVENUE_ARTICLE_SUMMARY
+              ),
+            })}
+          />
+          <div className="flex gap-4">
+            <FormSelect
+              selectClassName={inputClassName}
+              wrapperClassName={wrapperClassName}
+              label={textsConfig.FORM.WRITER_REQUEST.INPUTS.ARTICLE_ROLE}
+              errorMessage={errors.articleRole?.message}
+              options={
+                textsConfig.FORM.WRITER_REQUEST.SELECT_OPTIONS.ARTICLE_ROLE
+              }
+              placeholder={`${textsConfig.FORM.WRITER_REQUEST.INPUTS.ARTICLE_ROLE}を入力`}
+              {...register("articleRole", {
+                required: GetRequiredMessage(
+                  textsConfig.FORM.WRITER_REQUEST.INPUTS.ARTICLE_ROLE
+                ),
+              })}
+            />
+            <FormInput
+              inputClassName={inputClassName}
+              wrapperClassName={wrapperClassName}
+              label={textsConfig.FORM.WRITER_REQUEST.INPUTS.REFERENCE_URLS}
+              errorMessage={errors.referenceUrls?.message}
+              placeholder={`${textsConfig.FORM.WRITER_REQUEST.INPUTS.REFERENCE_URLS}を入力`}
+              {...register("referenceUrls", {
+                required: GetRequiredMessage(
+                  textsConfig.FORM.WRITER_REQUEST.INPUTS.REFERENCE_URLS
+                ),
+              })}
+            />
+          </div>
           <p>{textsConfig.FORM.WRITER_REQUEST.SECTIONS[2]}</p>
           <div className="flex gap-4">
             <FormInput
