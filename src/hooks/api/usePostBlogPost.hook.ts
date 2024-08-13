435370e562aeddc73d1ce5a2e9e8d02d5f9347f9
tzptzptzptzptzp <git@tzp.pink>
2024-08-13 18:41:23 +0900

    ✨ 記事生成フックを作成

---
+++ src/hooks/api/usePostBlogPost.hook.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+
+const postBlogPost = async ({
+  title,
+  id,
+  prompt,
+}: {
+  title: string;
+  id: number;
+  prompt: string;
+}) => {
+  return await axios.post(`/api/writer/${id}/blog-post`, {
+    title,
+    prompt,
+  });
+};
+
+export const usePostBlogPost = () => {
+  const { mutate } = useMutation({ mutationFn: postBlogPost });
+  return mutate;
+};
