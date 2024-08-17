989bd5f51f93818b4166fe82a9b98182b37303fe
tzptzptzptzptzp <git@tzp.pink>
2024-08-17 18:34:46 +0900

    🚸 記事をコピーするボタンを追加

--- src/components/features/Writer/ShowBlogPostModal/ShowBlogPostModal.feature.tsx
+++ src/components/features/Writer/ShowBlogPostModal/ShowBlogPostModal.feature.tsx
@@ -1,15 +1,40 @@
+import { toast } from "react-toastify";
 import { Border } from "@/components/atoms/Border/Border.atom";
+import { Button } from "@/components/atoms/Button/Button.atom";
 import { Loader } from "@/components/atoms/Loader/Loader.atom";
 import { CustomReactMarkdown } from "@/components/organisms/CustomReactMarkdown/CustomReactMarkdown.organism";
 import { ModalInner } from "@/components/templates/ModalInner/ModalInner.template";
 import { textsConfig } from "@/config/texts.config";
 import { useBlogPost } from "@/hooks/features/useBlogPost.hook";
+import { Icons } from "@/icons";
 
-const Header = ({ title }: { title: string }) => {
+const Header = ({
+  copyContent,
+  title,
+}: {
+  copyContent?: string;
+  title: string;
+}) => {
+  const handleCopy = () => {
+    if (!copyContent) return;
+    navigator.clipboard
+      .writeText(copyContent)
+      .then(() => {
+        toast(textsConfig.TOAST.CLIPBOARD_SAVE.SUCCESS);
+      })
+      .catch(() => {
+        toast.error(textsConfig.TOAST.CLIPBOARD_SAVE.ERROR);
+      });
+  };
   return (
     <div className="flex items-center gap-2">
       <Border className="-translate-y-[1px] w-4 bg-primary" />
       <p className="min-w-fit text-[22px]">{title}</p>
+      {copyContent && (
+        <Button onClick={handleCopy}>
+          <Icons.Copy className="stroke-2" width={22} height={22} />
+        </Button>
+      )}
       <Border className="-translate-y-[1px] bg-primary" />
     </div>
   );
@@ -29,11 +54,17 @@ export const ShowBlogPostModal = () => {
       {currentBlogPost.id !== 0 ? (
         <div className="flex flex-col gap-6">
           <div>
-            <Header title={textsConfig.MODAL.BLOG_POST.DESCRIPTION} />
+            <Header
+              copyContent={currentBlogPost.description}
+              title={textsConfig.MODAL.BLOG_POST.DESCRIPTION}
+            />
             <p>{currentBlogPost.description}</p>
           </div>
           <div>
-            <Header title={textsConfig.MODAL.BLOG_POST.ARTICLE} />
+            <Header
+              copyContent={currentBlogPost.content}
+              title={textsConfig.MODAL.BLOG_POST.ARTICLE}
+            />
             <CustomReactMarkdown markdown={currentBlogPost.content} />
           </div>
           <div>
