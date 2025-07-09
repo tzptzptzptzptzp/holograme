dde6894ec510df61439bad624949b18833cddf3b
tzptzptzptzptzp <git@tzp.pink>
2025-07-09 22:59:40 +0900

    🔧 GlobalFrameコンポーネントの構造を簡素化

--- src/components/templates/GlobalFrame/GlobalFrame.template.tsx
+++ src/components/templates/GlobalFrame/GlobalFrame.template.tsx
@@ -1,52 +1,19 @@
 "use client";
 import { Bounce, ToastContainer } from "react-toastify";
-import { useRecoilValue } from "recoil";
-import { Avatar } from "@/components/atoms/Avatar/Avatar.atom";
 import { Background } from "@/components/atoms/Background/Background.atom";
 import { FrameShadow } from "@/components/atoms/FrameShadow/FrameShadow.atom";
 import { Modal } from "@/components/organisms/Modal/Modal.organism";
-import { Navigation } from "@/components/organisms/Navigation/Navigation. organism";
-import { SelectedContentState } from "@/recoil/atoms.recoil";
-import { cn } from "@/utils/Cn.util";
 
 type Props = {
   children: React.ReactNode;
-  contents?: boolean;
-  shadow?: boolean;
 };
 
-export const GlobalFrame = ({
-  children,
-  contents = true,
-  shadow = true,
-}: Props) => {
-  const currentContent = useRecoilValue(SelectedContentState);
-
+export const GlobalFrame = ({ children }: Props) => {
   return (
     <div className="flex items-center justify-center relative z-0 w-screen h-screen s:h-svh p-12 s:p-2">
       <div className="relative z-0 overflow-hidden w-full h-full rounded-3xl isolate">
         <Background />
-        <div className="flex s:flex-col z-10 w-full h-full pr-20 s:pr-0 s:pb-[46px]">
-          {contents && (
-            <div className="flex s:hidden items-end relative top-0 w-[50%] h-full pl-4">
-              <Avatar />
-            </div>
-          )}
-          <main
-            className={cn(
-              "flex flex-col items-center justify-center flex-1 s:flex-auto relative top-0 s:z-10 w-[50%] s:w-full min-w-[600px] s:min-w-0 h-full pr-10 pl-4 s:p-4",
-              shadow && "u-shadow-sm"
-            )}
-          >
-            {children}
-          </main>
-          {currentContent === "home" && (
-            <div className="hidden s:flex flex-none items-end justify-center relative top-0 z-0 h-full max-h-[62svh] pl-4 s:px-2">
-              <Avatar />
-            </div>
-          )}
-          {contents && <Navigation />}
-        </div>
+        <main className="flex s:flex-col z-10 w-full h-full">{children}</main>
         <FrameShadow />
         <ToastContainer
           className="u-shadow w-fit"
