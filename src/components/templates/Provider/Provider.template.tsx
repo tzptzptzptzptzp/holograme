d93735411553e7ef235439ca3bfdfc425c9bfdae
tzptzptzptzptzp <git@tzp.pink>
2025-07-10 23:23:27 +0900

    🔥 recoil関連ファイルを削除

--- src/components/templates/Provider/Provider.template.tsx
+++ src/components/templates/Provider/Provider.template.tsx
@@ -1,12 +1,9 @@
 "use client";
-import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const Provider = ({ children }: { children: React.ReactNode }) => {
   const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>{children}</RecoilRoot>
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 };
