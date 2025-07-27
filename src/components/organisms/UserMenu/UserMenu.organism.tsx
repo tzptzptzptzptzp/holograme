495f1fab009ed7ef15511b889f7910a923178366
tzptzptzptzptzp <git@tzp.pink>
2025-07-27 11:44:05 +0900

    🚸 モバイルでのユーザーメニューの表示方法を改善

--- src/components/organisms/UserMenu/UserMenu.organism.tsx
+++ src/components/organisms/UserMenu/UserMenu.organism.tsx
@@ -16,10 +16,10 @@ export const UserMenu = ({ isHovered }: Props) => {
   return (
     <div
       className={cn(
-        "absolute right-full s:-right-4 pr-10 s:pr-0 duration-300",
+        "absolute right-full s:-top-14 s:-translate-y-1/2 pr-10 s:pr-0 duration-300",
         isHovered
-          ? "u-centering-y s:-top-[2.35rem] opacity-1 pointer-events-auto"
-          : "top-full opacity-0 pointer-events-none"
+          ? "top-1/2 -translate-y-1/2 s:-right-6 opacity-1 pointer-events-auto"
+          : "top-full s:-right-24 opacity-0 pointer-events-none"
       )}
     >
       <ul className="min-w-[200px] px-3 py-2 rounded-lg bg-white shadow-sm">
