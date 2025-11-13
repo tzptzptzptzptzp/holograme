aba29712f2bbc76a1e1850e54df62f3782e18717
tzptzptzptzptzp <git@tzp.pink>
2025-11-13 23:41:02 +0900

    ✨ クライアントサイドで取得したデータをStoreに格納する処理を追加

--- src/hooks/useInitialize.hook.ts
+++ src/hooks/useInitialize.hook.ts
@@ -1,7 +1,20 @@
 import { useEffect } from "react";
 import { useSearchTypeStore, SearchType } from "@/stores/searchType.store";
+import { useGetUser } from "./api/useGetUser.hook";
+import { useUserStore } from "@/stores/user.store";
 
 export const useInitialize = () => {
+  // ユーザーデータの初期化
+  const { data: userData } = useGetUser();
+  const { setUser } = useUserStore();
+
+  useEffect(() => {
+    if (userData) {
+      setUser(userData);
+    }
+  }, [userData, setUser]);
+
+  // 検索タイプの初期化
   const setSearchType = useSearchTypeStore((state) => state.setSearchType);
 
   useEffect(() => {
