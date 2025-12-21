8468f257113a69f46692c45f11790a63ee5be8aa
tzptzptzptzptzp <git@tzp.pink>
2025-12-21 23:57:39 +0900

    🚑 ビルドエラーを解消

--- src/stores/tweet.store.ts
+++ src/stores/tweet.store.ts
@@ -1,21 +1,11 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
-import { STORAGE_KEYS } from "@/configs/storage.config";
 
 type TweetStoreType = {
   tweet: string;
   setTweet: (tweet: string) => void;
 };
 
-export const useTweetStore = create<TweetStoreType>()(
-  persist(
-    (set) => ({
-      tweet: "",
-      setTweet: (tweet) => set({ tweet }),
-    }),
-    {
-      name: STORAGE_KEYS.TWEET,
-      partialize: (state) => ({ tweet: state.tweet }),
-    }
-  )
-);
+export const useTweetStore = create<TweetStoreType>()((set) => ({
+  tweet: "",
+  setTweet: (tweet) => set({ tweet }),
+}));
