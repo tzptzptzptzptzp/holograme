fd204263e24cb1d8d3d6a16ea8148df32ed21a04
tzptzptzptzptzp <git@tzp.pink>
2026-04-11 23:59:25 +0900

    ⚡️ バージョン更新に伴う諸々の改善

--- src/utils/GlobalFetch.util.ts
+++ src/utils/GlobalFetch.util.ts
@@ -2,7 +2,7 @@ import { ServerSideFetch } from "./ServerSideFetch.util";
 import { GlobalDataType } from "@/types";
 
 export const GlobalFetch = async () => {
-  const { apiFetch } = ServerSideFetch();
+  const { apiFetch } = await ServerSideFetch();
   const initializeData: GlobalDataType = await apiFetch("/api/initialize");
 
   return initializeData;
--- src/utils/ServerSideFetch.util.ts
+++ src/utils/ServerSideFetch.util.ts
@@ -1,12 +1,13 @@
 import { headers } from "next/headers";
 import { ofetch } from "ofetch";
 
-export const ServerSideFetch = () => {
+export const ServerSideFetch = async () => {
+  const requestHeaders = await headers();
   const apiFetch = ofetch.create({
     baseURL: process.env.API_BASE_URL,
     timeout: 100000,
     onRequest({ options }) {
-      options.headers = headers();
+      options.headers = requestHeaders;
       options.headers = new Headers(options.headers);
       const cookieHeader = options.headers.get("cookie");
 
